fb1fc282f72be60cf3a0420347f412ec20599264
User <user@usermail.com>
2022-02-19 16:32:17 +0200

    Complete exercise

--- gigasecond/src/lib.rs
+++ gigasecond/src/lib.rs
@@ -1,8 +1,9 @@
+use std::time::Duration;
 use time::PrimitiveDateTime as DateTime;
 
 // Returns a DateTime one billion seconds after start.
 pub fn after(start: DateTime) -> DateTime {
-    start + 100000000_u64.seconds()
-    
-    // unimplemented!("What time is a gigasecond later than {}", start);
+    let duration = Duration::new(1_000_000_000_u64, 0);
+    let new_start = start + duration;
+    return new_start;
 }
